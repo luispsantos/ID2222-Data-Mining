e3ed1b259a66b630417cdf71d181cf3e36416337
Luis Santos <lsantos.dev@gmail.com>
2020-11-05 21:07:20 +0000

    improving the read dataset function (reading directly from zipfile)

--- homework 1/dataset_reader.py
+++ homework 1/dataset_reader.py
@@ -1,19 +1,28 @@
+import zipfile
 import itertools
 import jsonlines
 
 
-def read_webhose_dataset(inputfile, num_docs):
+def read_webhose_dataset(dataset_file, num_docs=None):
     docs = []
-    with jsonlines.open(input_file) as reader:
-        # read only the first `num_docs` documents
-        for doc in itertools.islice(reader, num_docs):
-            docs.append(doc['text'])
+    # read the zip file containing the dataset
+    with zipfile.ZipFile(dataset_file) as dataset_zipfile:
+        # find the name of the JSON file inside the zip file
+        json_filename = dataset_zipfile.namelist()[0]
+
+        with dataset_zipfile.open(json_filename) as json_file:
+            # each line in the file is a JSON document
+            json_reader = jsonlines.Reader(json_file)
+
+            # read only the initial num_docs documents
+            for doc in itertools.islice(json_reader, num_docs):
+                docs.append(doc['text'])
 
     return docs
 
 
-num_docs = 10
-input_file = '16125_webhose_2020_02_2eb7927911a2ee90f88fb57c2721c4ac_0000001.json'
+dataset_file = 'datasets/covid-discussions.zip'
+num_docs = 100
 
 docs = read_webhose_dataset(input_file, num_docs)
 print(docs)
