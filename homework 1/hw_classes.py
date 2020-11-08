00c8604016553b0c20492eab9af16bae68bbfd70
Luis Santos <lsantos.dev@gmail.com>
2020-11-08 11:15:44 +0000

    adding TextPreprocessor and CSR sparse matrix

--- homework 1/hw_classes.py
+++ homework 1/hw_classes.py
@@ -1,7 +1,39 @@
 import itertools
 import operator
 import numpy as np
+import string
 import sympy
+import unicodedata
+from scipy.sparse import csr_matrix
+
+
+class TextPreprocessor:
+    def __init__(self, lowercase=True, remove_punctuation=True, remove_accents=True, normalize_whitespace=True):
+        self.lowercase = lowercase
+        self.remove_accents = remove_accents
+        self.remove_punctuation = remove_punctuation
+        self.normalize_whitespace = normalize_whitespace
+
+    def strip_accents(self, doc):
+        doc_nfkd = unicodedata.normalize('NFKD', doc)
+        doc_ascii = doc_nfkd.encode('ASCII', 'ignore').decode('ascii')
+
+        return doc_ascii
+
+    def preprocess_document(self, doc):
+        if self.lowercase:
+            doc = doc.lower()
+
+        if self.remove_accents:
+            doc = self.strip_accents(doc)
+
+        if self.remove_punctuation:
+            doc = ''.join(char for char in doc if char not in string.punctuation)
+
+        if self.normalize_whitespace:
+            doc = ' '.join(doc.split())
+
+        return doc
 
 
 class Shingling:
@@ -26,8 +58,20 @@ class Shingling:
 
     def create_document_shingles(self, doc_list):
         doc_shingles = [self.create_unique_shingles(doc) for doc in doc_list]
-        n_shingles = len(set(shingle for shingles in doc_shingles for shingle in shingles))
-        return doc_shingles, n_shingles
+        # obtain a mapping of {shingle -> shingle index} over all shingles
+        unique_shingles = set(shingle for shingles in doc_shingles for shingle in shingles)
+        shingle_idxs = {shingle: idx for idx, shingle in enumerate(sorted(unique_shingles))}
+        return doc_shingles, shingle_idxs
+
+    def create_characteristic_matrix(self, doc_list):
+        doc_shingles, shingle_idxs = self.create_document_shingles(doc_list)
+        n_docs, n_shingles = len(doc_shingles), len(shingle_idxs)
+
+        l = [(shingle_idxs[shingle], doc_id, 1) for doc_id, shingles in enumerate(doc_shingles) for shingle in shingles]
+        shingle_indices, doc_indices, data = zip(*l)
+
+        char_matrix = csr_matrix((data, (shingle_indices, doc_indices)), shape=(n_shingles, n_docs), dtype=np.bool_)
+        return char_matrix
 
 
 class CompareSets:
@@ -76,6 +120,27 @@ class MinHashing:
     def compute_universal_hash(self, x, a, b, p, m):
         # for more information check: https://en.wikipedia.org/wiki/Universal_hashing#Hashing_integers
         return ((a*x + b) % p) % m
+
+    def compute_signature_perm(self, doc_shingles, n_shingles):
+        n_signature, n_docs = self.n_signature, len(doc_shingles)
+
+        unique_shingles = set(shingle for shingles in doc_shingles for shingle in shingles)
+        shingle_idxs = {shingle: idx for idx, shingle in enumerate(sorted(unique_shingles))}
+
+        l = [(shingle_idxs[shingle], doc_id) for doc_id, doc in enumerate(doc_shingles) for shingle in doc]
+        a, b = zip(*l)
+
+        M = csr_matrix(([1]*len(a), (a, b)), shape=(n_shingles, n_docs), dtype=np.bool_)
+        signature = np.zeros((n_signature, n_docs), dtype=np.int32)
+
+        for i in range(n_signature):
+            rand_idxs = np.random.permutation(n_shingles)
+            M_permuted = M[rand_idxs, :]
+
+            signature[i, :] = np.argmax(M_permuted, axis=0)
+
+        return signature
+
 
 class CompareSignatures:
 
