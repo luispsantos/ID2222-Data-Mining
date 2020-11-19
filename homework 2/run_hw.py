9df938074c67e6bcb4dffe0dca7f60d9f3d652d5
Luis Santos <lsantos.dev@gmail.com>
2020-11-19 09:46:14 +0000

    small correction

--- homework 2/run_hw.py
+++ homework 2/run_hw.py
@@ -5,9 +5,9 @@ from hw_classes import Apriori, AssociationRules
 
 parser = argparse.ArgumentParser(description='Find frequent itemsets and association rules for a given support/confidence')
 parser.add_argument('-dataset-file', default='T10I4D100K.dat', help='name of a transactions dataset with baskets and items')
-parser.add_argument('-s', default=1000, type=int, help='minimum support a itemset must have to be considered frequen')
+parser.add_argument('-s', default=1000, type=int, help='minimum support a itemset must have to be considered frequent')
 parser.add_argument('-c', default=0.5, type=float, help='minimum confidence a rule must have to be generated')
-parser.add_argument('-verbose', default=True, type=bool, help='decides if the results gets printed')
+parser.add_argument('-verbose', default=True, type=bool, help='decides if the results are printed')
 
 
 args = parser.parse_args()
