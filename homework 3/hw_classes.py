267efdc80b80cd2ef18c111f2ccff3cfc6c70d49
Luis Santos <lsantos.dev@gmail.com>
2020-11-22 23:57:23 +0000

    implementation of TriestImpr

--- homework 3/hw_classes.py
+++ homework 3/hw_classes.py
@@ -131,3 +131,75 @@ class TriestBase:
             print(f'Global triangles estimate: {global_triangles}')
 
         return global_triangles
+
+
+class TriestImpr:
+    """Implementation of the Triest-Impr Algorithm"""
+
+    def __init__(self, M, verbose=False):
+        self.M = M
+        self.verbose = verbose
+        self.subgraph = SubGraph(verbose)
+        self.global_counter = 0
+        self.local_counters = defaultdict(int)
+
+    def sample_edge(self, t):
+        # base case of reservoir sampling - always inserts the new edge
+        if t <= self.M:
+            return True
+
+        # simulates flipping a biased coin with the heads probability M/t
+        elif random.random() < self.M / t:
+            # obtain a random edge from our graph
+            edge_list = self.subgraph.get_edges()
+            w, z = random.choice(edge_list)
+
+            # remove the sampled edge from subgraph
+            self.subgraph.remove_edge(w, z)
+            return True
+
+        return False
+
+    def update_counters(self, t, u, v):
+        # make sure the subgraph contains both nodes the edge leads to
+        if not self.subgraph.has_node(u) or not self.subgraph.has_node(v):
+            return
+
+        neighbors_u = self.subgraph.get_neighbors(u)
+        neighbors_v = self.subgraph.get_neighbors(v)
+
+        shared_neighbors = neighbors_u & neighbors_v
+        incr_value = self.compute_eta(t)
+
+        # update the global/local counters of the shared neighborhood
+        for c in shared_neighbors:
+            self.global_counter += incr_value
+            self.local_counters[c] += incr_value
+            self.local_counters[u] += incr_value
+            self.local_counters[v] += incr_value
+
+    def compute_eta(self, t):
+        return max(1, (t-1)*(t-2) / (self.M*(self.M-1)))
+
+    def algorithm(self, dataset_file):
+        edge_stream = read_snap_dataset(dataset_file)
+        t = 0
+
+        for u, v in edge_stream:
+            if u == v:
+                continue
+            elif u > v:
+                v, u = u, v
+            if self.subgraph.has_edge(u, v):
+                continue
+
+            t += 1
+            self.update_counters(t, u, v)
+            if self.sample_edge(t):
+                self.subgraph.add_edge(u, v)
+
+        global_triangles = int(self.global_counter)
+        if self.verbose:
+            print(f'Global triangles estimate: {global_triangles}')
+
+        return global_triangles
