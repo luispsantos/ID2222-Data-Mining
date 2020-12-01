e7e482420b43228a5925ae1eed14e07f9916e5e1
Luis Santos <lsantos.dev@gmail.com>
2020-12-01 10:35:45 +0000

    cleaning a bit the graph spectra code

--- homework 4/Graph_Spectra.m
+++ homework 4/Graph_Spectra.m
@@ -16,16 +16,16 @@ function [clusters, L]=algorithm(E, k)
     A=step1(E);
     figure(1)
     spy(A)
-    figure(5)
-    plot(graph(A), 'Layout','force');
     figure(2)
+    plot(graph(A), 'Layout','force');
+    %figure(3)
     %h=plot(graph(A), 'Layout','force');
-    %figure(3)    
+    figure(3)
     h=plot(graph(A),'Layout','force3')
-    
+
     % step 2
     L=step2(A);
-    
+
     % step 3
     [V,d]=eigs(L,k);
 
@@ -34,38 +34,35 @@ function [clusters, L]=algorithm(E, k)
 
     % step 5
     clusters=kmeans(Y,k);
-    
+
     % step 6
     cluster_colors=hsv(k);
     for i=1:k
         cluster_members=find(clusters==i);
         highlight(h, cluster_members , 'NodeColor', cluster_colors(i,:))
     end
-    figure(3)
+    figure(4)
     [F_V,~]=eigs(un_normalized_L(A),2,'SA');
-    %[F_V,~]=eigs(L,2,'SA');
+    plot(sort(F_V(:,2)),'-*')
+    figure(5)
+    [F_V,~]=eigs(L,2,'SA');
     plot(sort(F_V(:,2)),'-*')
 end
 
-function A= step1(E)
+function A=step1(E)
     col1=E(:,1);
     col2=E(:,2);
-    %figure(1)
-    %plot(col1,col2,'.')
     max_ids = max(max(col1,col2));
-    As= sparse(col1, col2, 1, max_ids, max_ids); 
+    As= sparse(col1, col2, 1, max_ids, max_ids);
     A= full(adjacency(graph(As)));
-end 
+end
 
 function L=step2(A)
     D=diag(sum(A,2));
     L=(D^(-1/2)*A*D^(-1/2));
-end 
+end
 
 function L=un_normalized_L(A)
     D=diag(sum(A,2));
     L=D-A;
 end
-
-
-
