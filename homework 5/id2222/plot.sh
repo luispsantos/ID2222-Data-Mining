0009f728f51a7bae1e3988e3c78e6bcb3fb6b1ff
Luis Santos <lsantos.dev@gmail.com>
2020-12-08 21:38:15 +0000

    adding script to run all tasks

--- homework 5/id2222/plot.sh
+++ homework 5/id2222/plot.sh
@@ -2,7 +2,7 @@
 
 #check for installation of parallel-rsync
 
-if [ "$#" -ne 1 ] ; then
+if [ "$#" -lt 1 ] ; then
 	echo "Data file not supplied."
 	echo "Usage ./plot {data-file.txt}"
 	exit
@@ -10,4 +10,9 @@ fi
 
 gnuplot -e "filename='$1'" graph.gnuplot
 
-xdg-open graph.png
+if [ "$#" -lt 2 ] ; then
+  xdg-open graph.png
+else
+  echo "Creating graph at $2"
+  mv graph.png $2
+fi
